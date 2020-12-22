c3374d6bcbddee98b9768893ae1227be1425b8f3
Andrés <andres.gerz@gmail.com>
2020-12-21 23:46:54 -0300

    I installed lando laravel default

---
+++ api/database/factories/CoursesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\courses;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CoursesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = courses::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
